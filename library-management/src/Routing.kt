b82a31d9dc52daeeebaa520f27283249b42a1a7c
Luca Zeolla <zqrj1548@leeds.ac.uk>
2026-02-14 16:41:39 +0000

    Resolved merge conflicts

--- library-management/src/Routing.kt
+++ library-management/src/Routing.kt
@@ -1,4 +1,6 @@
+// Set up application routing & request handling
 package com.example
+<<<<<<< Updated upstream
 
 import com.example.repository.BookRepository
 import com.example.repository.ReservationRepository
@@ -10,6 +12,25 @@ import io.ktor.server.pebble.respondTemplate
 import io.ktor.server.request.receiveParameters
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.*
+=======
+import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationCall
+import io.ktor.server.application.log
+import io.ktor.server.auth.UserIdPrincipal
+import io.ktor.server.auth.UserPasswordCredential
+import io.ktor.server.auth.authenticate
+import io.ktor.server.auth.principal
+import io.ktor.server.pebble.respondTemplate
+import io.ktor.server.request.receiveParameters
+import io.ktor.server.response.respondRedirect
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.routing
+import io.ktor.server.sessions.clear
+import io.ktor.server.sessions.sessions
+import io.ktor.server.sessions.set
+import io.ktor.server.util.getOrFail
+>>>>>>> Stashed changes
 
 import org.jetbrains.exposed.sql.transactions.transaction
 import com.example.database.Book
@@ -22,8 +43,16 @@ fun Application.configureRouting() {
     val reservationService = ReservationService(ReservationRepository())
 
     routing {
-        get("/") {
-            call.displayForm()
+        get("/") { call.homePage() }
+        get("/register") { call.registrationPage() }
+        post("/register") { call.registerUser() }
+        get("/login") { call.loginPage() }
+        authenticate("auth-form") {
+            post("/login") { call.login() }
+        }
+        authenticate("auth-session") {
+            get("/private") { call.privatePage() }
+            get("/logout") { call.logout() }
         }
         get("/books") {
             call.displayBooks()
@@ -110,10 +139,11 @@ fun Application.configureRouting() {
     }
 }
 
-private suspend fun ApplicationCall.displayForm() {
-    respondTemplate("form.peb", model = emptyMap<String, Any>())
+private suspend fun ApplicationCall.homePage() {
+    respondTemplate("index.peb", model = mapOf("users" to UserDatabase.size))
 }
 
+<<<<<<< Updated upstream
 private suspend fun ApplicationCall.displayBooks() {
     val books = transaction {
         Book.all().toList()
@@ -134,3 +164,59 @@ private suspend fun ApplicationCall.displayCopies() {
     }
     respondTemplate("copies.peb", mapOf("copies" to copiesForView))
 }
+=======
+private suspend fun ApplicationCall.registrationPage() {
+    respondTemplate("register.peb", model = emptyMap())
+}
+
+private suspend fun ApplicationCall.registerUser() {
+    val credentials = getCredentials()
+    val result = runCatching {
+        UserDatabase.addUser(credentials)
+    }
+    if (result.isSuccess) {
+        application.log.info("User ${credentials.name} registered")
+        respondRedirect("/")
+    }
+    else {
+        val error = result.exceptionOrNull()?.message ?: ""
+        respondTemplate("register.peb", model = mapOf("error" to error))
+    }
+}
+
+private suspend fun ApplicationCall.getCredentials(): UserPasswordCredential {
+    val formParams = receiveParameters()
+    val username = formParams.getOrFail("username")
+    val password = formParams.getOrFail("password")
+    return UserPasswordCredential(username, password)
+}
+
+private suspend fun ApplicationCall.loginPage() {
+    respondTemplate("login.peb", model = emptyMap())
+}
+
+private suspend fun ApplicationCall.login() {
+    val username = principal<UserIdPrincipal>()?.name.toString()
+    application.log.info("User $username logged in")
+    sessions.set(UserSession(username, 1))
+    respondRedirect("/private")
+}
+
+private suspend fun ApplicationCall.privatePage() {
+    val session = principal<UserSession>()
+    // Increment visit count & update session cookie
+    sessions.set(session?.copy(count = session.count + 1))
+
+    respondTemplate("private.peb", model = mapOf(
+        "username" to session?.username.toString(),
+        "visits" to (session?.count ?: 0),
+    ))
+}
+
+private suspend fun ApplicationCall.logout() {
+    val username = principal<UserSession>()?.username.toString()
+    application.log.info("User $username logged out")
+    sessions.clear<UserSession>()
+    respondRedirect("/")
+}
+>>>>>>> Stashed changes
